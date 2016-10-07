e9ed7fdf35a21e0b0fc68d231957aa3a70823bca
Diogo Freitas <diogo.maria.freitas@gmail.com>
2016-10-07 09:36:59 +0100

    Adjustments Class #3

--- js/alliedship.js
+++ js/alliedship.js
@@ -8,77 +8,101 @@
  */
 
 var AShip;
+var	material1 = new THREE.MeshBasicMaterial({ color: 0xbfbfbf, wireframe: false });
+var material2 = new THREE.MeshBasicMaterial({ color: 0x404040, wireframe: false });
 
 function createAlliedShip(x, y, z) {
 	AShip = new THREE.Object3D();
   	AShip.userData = {direction: 0, velocity: 0, aceleration: 500};
-	material = new THREE.MeshBasicMaterial({ color: 0xbfbfbf, wireframe: false });
 
 	addASCenter(AShip, 0, 0, 0);
+	addASPropulsor(AShip, 0, 0, 10);
 	addASCanon(AShip, 0, 0, -30);
 	addASGun(AShip, 8, 0, -48);
 	addASGun(AShip, -8, 0, -48);
 	addASGunCockpit1(AShip, -10, -5, -30);
 	addASGunCockpit2(AShip, 10, -5, -30);
+	/*addASLeg(AShip, 0, 0, 0);*/
 
 	scene.add(AShip);
 	AShip.position.x = x;
 	AShip.position.y = y;
 	AShip.position.z = z;
-}
 
-function addASCenter(obj, x, y, z) {
-	geometry = new THREE.CylinderGeometry(40, 40, 10, 100, 10, false);
-	mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(x, y, z);
+	function addASCenter(obj, x, y, z) {
+		geometry = new THREE.CylinderGeometry(40, 40, 10, 100, 10, false);
+		mesh = new THREE.Mesh(geometry, material1);
+		mesh.position.set(x, y, z);
 
-	obj.add(mesh);
-}
+		obj.add(mesh);
+	}
 
-function addASCanon(obj, x, y, z) {
-	geometry = new THREE.CubeGeometry(10, 10, 35);
-	mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(x, y, z);
+	function addASPropulsor(obj, x, y, z) {
+		geometry = new THREE.CylinderGeometry(35, 35, 7, 100, 10, false);
+		mesh = new THREE.Mesh(geometry, material2);
+		mesh.position.set(x, y, z);
 
-	obj.add(mesh);
-}
+		obj.add(mesh);
+	}
 
-function addASGun(obj, x, y, z) {
-	geometry = new THREE.CubeGeometry(4, 10, 36);
-	mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(x, y, z);
+	function addASCanon(obj, x, y, z) {
+		geometry = new THREE.CubeGeometry(10, 10, 35);
+		mesh = new THREE.Mesh(geometry, material1);
+		mesh.position.set(x, y, z);
 
-	obj.add(mesh);
-}
+		obj.add(mesh);
+	}
 
-function addASGunCockpit1(obj, x, y, z) {
-	var triangleShape = new THREE.Shape();
-	triangleShape.moveTo(0, 0);
-	triangleShape.lineTo(0, 16);
-	triangleShape.lineTo(36, 0);
-	triangleShape.lineTo(0, 0);
+	function addASGun(obj, x, y, z) {
+		geometry = new THREE.CubeGeometry(4, 10, 36);
+		mesh = new THREE.Mesh(geometry, material1);
+		mesh.position.set(x, y, z);
 
-	geometry = new THREE.ExtrudeGeometry(triangleShape, { amount: 10, bevelEnabled: false });
-	mesh = new THREE.Mesh(geometry, material);
-	mesh.rotation.x = -Math.PI/2;
-	mesh.rotation.z = Math.PI/2;
-	mesh.position.set(x, y, z);
+		obj.add(mesh);
+	}
 
-	obj.add(mesh);
-}
+	function addASGunCockpit1(obj, x, y, z) {
+		var triangleShape = new THREE.Shape();
+		triangleShape.moveTo(0, 0);
+		triangleShape.lineTo(0, 16);
+		triangleShape.lineTo(36, 0);
+		triangleShape.lineTo(0, 0);
 
-function addASGunCockpit2(obj, x, y, z) {
-	var triangleShape = new THREE.Shape();
-	triangleShape.moveTo(0, 0);
-	triangleShape.lineTo(0, 16);
-	triangleShape.lineTo(36, 0);
-	triangleShape.lineTo(0, 0);
+		geometry = new THREE.ExtrudeGeometry(triangleShape, { amount: 10, bevelEnabled: false });
+		mesh = new THREE.Mesh(geometry, material1);
+		mesh.rotation.x = -Math.PI/2;
+		mesh.rotation.z = Math.PI/2;
+		mesh.position.set(x, y, z);
 
-	geometry = new THREE.ExtrudeGeometry(triangleShape, { amount: -10, bevelEnabled: false });
-	mesh = new THREE.Mesh(geometry, material);
-	mesh.rotation.x = Math.PI/2;
-	mesh.rotation.z = -Math.PI/2;
-	mesh.position.set(x, y, z);
+		obj.add(mesh);
+	}
 
-	obj.add(mesh);
+	function addASGunCockpit2(obj, x, y, z) {
+		var triangleShape = new THREE.Shape();
+		triangleShape.moveTo(0, 0);
+		triangleShape.lineTo(0, 16);
+		triangleShape.lineTo(36, 0);
+		triangleShape.lineTo(0, 0);
+
+		geometry = new THREE.ExtrudeGeometry(triangleShape, { amount: -10, bevelEnabled: false });
+		mesh = new THREE.Mesh(geometry, material1);
+		mesh.rotation.x = Math.PI/2;
+		mesh.rotation.z = -Math.PI/2;
+		mesh.position.set(x, y, z);
+
+		obj.add(mesh);
+	}
+
+	function addASLeg(obj, x, y, z) {
+		geometry = new THREE.CubeGeometry(4, 1, 4);
+		mesh = new THREE.Mesh(geometry, material1);
+		mesh.position.set(x, (y - 2.5), z);
+
+		geometry1 = new THREE.CylinderGeometry(2, 2, 4, 20, 4, false);
+		mesh1 = new THREE.Mesh(geometry1, material1);
+		mesh1.position.set(x, y, z);
+
+		obj.add(mesh);
+		obj.add(mesh1);
+	}
 }
--- js/enemieship.js
+++ js/enemieship.js
@@ -17,54 +17,52 @@ function createEnemieShip(x, y, z) {
 	addESWing(ship, -39, -30, -17.5);
 	addESWing(ship, 39, -30, -17.5);
 
-	//ship.rotation.y = Math.PI/2;
-
 	scene.add(ship);
 	ship.position.x = x;
 	ship.position.y = y;
 	ship.position.z = z;
-}
 
-function addESCockpit(obj, x, y, z) {
-	geometry = new THREE.SphereGeometry(20, 100, 100);
-	mesh = new THREE.Mesh(geometry, material);
-	mesh.position.set(x, y, z);
+	function addESCockpit(obj, x, y, z) {
+		geometry = new THREE.SphereGeometry(20, 100, 100);
+		mesh = new THREE.Mesh(geometry, material);
+		mesh.position.set(x, y, z);
 
-	obj.add(mesh);
-}
+		obj.add(mesh);
+	}
 
-function addESWingConector1(obj, x, y, z) {
-	geometry = new THREE.CylinderGeometry(5, 10, 25, 100, 10, false);
-	mesh = new THREE.Mesh(geometry, material);
-	mesh.rotation.z = -Math.PI/2;
-	mesh.position.set(x, y, z);
+	function addESWingConector1(obj, x, y, z) {
+		geometry = new THREE.CylinderGeometry(5, 10, 25, 100, 10, false);
+		mesh = new THREE.Mesh(geometry, material);
+		mesh.rotation.z = -Math.PI/2;
+		mesh.position.set(x, y, z);
 
-	obj.add(mesh);
-}
+		obj.add(mesh);
+	}
 
-function addESWingConector2(obj, x, y, z) {
-	geometry = new THREE.CylinderGeometry(5, 10, 25, 100, 10, false);
-	mesh = new THREE.Mesh(geometry, material);
-	mesh.rotation.z = Math.PI/2;
-	mesh.position.set(x, y, z);
+	function addESWingConector2(obj, x, y, z) {
+		geometry = new THREE.CylinderGeometry(5, 10, 25, 100, 10, false);
+		mesh = new THREE.Mesh(geometry, material);
+		mesh.rotation.z = Math.PI/2;
+		mesh.position.set(x, y, z);
 
-	obj.add(mesh);
-}
+		obj.add(mesh);
+	}
 
-function addESWing(obj, x, y, z) {
-	var hexagonShape = new THREE.Shape();
-	hexagonShape.moveTo(0, 0);
-	hexagonShape.lineTo(35, 0);
-	hexagonShape.lineTo(43, 30);
-	hexagonShape.lineTo(35, 60);
-	hexagonShape.lineTo(0, 60);
-	hexagonShape.lineTo(-8, 30);
-	hexagonShape.lineTo(0, 0);
+	function addESWing(obj, x, y, z) {
+		var hexagonShape = new THREE.Shape();
+		hexagonShape.moveTo(0, 0);
+		hexagonShape.lineTo(35, 0);
+		hexagonShape.lineTo(43, 30);
+		hexagonShape.lineTo(35, 60);
+		hexagonShape.lineTo(0, 60);
+		hexagonShape.lineTo(-8, 30);
+		hexagonShape.lineTo(0, 0);
 
-	geometry = new THREE.ExtrudeGeometry(hexagonShape, { amount: -2, bevelEnabled: false });
-	mesh = new THREE.Mesh(geometry, material);
-	mesh.rotation.y = -Math.PI/2;
-	mesh.position.set(x, y, z);
+		geometry = new THREE.ExtrudeGeometry(hexagonShape, { amount: -2, bevelEnabled: false });
+		mesh = new THREE.Mesh(geometry, material);
+		mesh.rotation.y = -Math.PI/2;
+		mesh.position.set(x, y, z);
 
-	obj.add(mesh);
+		obj.add(mesh);
+	}
 }
