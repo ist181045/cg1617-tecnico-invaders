049713d5967d9986ff13d69285d5a7cce007912b
Rui Ventura <RGCV@users.noreply.github.com>
2016-10-17 17:45:39 +0100

    General refactor, might not work

--- js/alliedship.js
+++ js/alliedship.js
@@ -2,14 +2,14 @@
  * CG Space Invaders
  * CG45179 16'17
  *
- * @author: Rui Ventura (ist181045)
- * @author: Diogo Freitas (ist181586)
- * @author: Sara Azinhal (ist181700)
+ * @author: Rui Ventura ( ist181045 )
+ * @author: Diogo Freitas ( ist181586 )
+ * @author: Sara Azinhal ( ist181700 )
  */
 
 class AlliedShip {
 
-	constructor(x, y, z) {
+	constructor ( x, y, z ) {
 		this.max_speed = 750;
 		this.acceleration = 400;
 		this.friction = 150;
@@ -18,122 +18,122 @@ class AlliedShip {
 
 		this.AS = new THREE.Object3D();
 
-		this.addASCenter(this.AS, 0, 0, 0);
-		this.addASPropulsor(this.AS, 0, 0, 10);
-		this.addASCanon(this.AS, 0, 0, -30);
-		this.addASGun(this.AS, 8, 0, -48);
-		this.addASGun(this.AS, -8, 0, -48);
-		this.addASGunCockpit1(this.AS, -10, -5, -30);
-		this.addASGunCockpit2(this.AS, 10, -5, -30);
-		//this.addASLeg(this.AS, 0, -9, 0);
+		this.addASCenter( this.AS, 0, 0, 0 );
+		this.addASPropulsor( this.AS, 0, 0, 10 );
+		this.addASCanon( this.AS, 0, 0, -30 );
+		this.addASGun( this.AS, 8, 0, -48 );
+		this.addASGun( this.AS, -8, 0, -48 );
+		this.addASGunCockpit1( this.AS, -10, -5, -30 );
+		this.addASGunCockpit2( this.AS, 10, -5, -30 );
+		//this.addASLeg( this.AS, 0, -9, 0 );
 
-		scene.add(this.AS);
+		scene.add( this.AS );
 		this.AS.position.x = x;
 		this.AS.position.y = y;
 		this.AS.position.z = z;
 	}
 
-	getObj() {
+	getObj () {
 		return this.AS;
 	}
 
-	addASCenter(obj, x, y, z) {
-		var geometry = new THREE.CylinderGeometry(40, 40, 10, 100, 10, false);
-		var mesh = new THREE.Mesh(geometry, material1);
-		mesh.position.set(x, y, z);
+	addASCenter ( obj, x, y, z ) {
+		var geometry = new THREE.CylinderGeometry( 40, 40, 10, 100, 10, false );
+		var mesh = new THREE.Mesh( geometry, material1 );
+		mesh.position.set( x, y, z );
 
-		obj.add(mesh);
+		obj.add( mesh );
 	}
 
-	addASPropulsor(obj, x, y, z) {
-		var geometry = new THREE.CylinderGeometry(35, 35, 7, 100, 10, false);
-		var mesh = new THREE.Mesh(geometry, material2);
-		mesh.position.set(x, y, z);
+	addASPropulsor ( obj, x, y, z ) {
+		var geometry = new THREE.CylinderGeometry( 35, 35, 7, 100, 10, false );
+		var mesh = new THREE.Mesh( geometry, material2 );
+		mesh.position.set( x, y, z );
 
-		obj.add(mesh);
+		obj.add( mesh );
 	}
 
-	addASCanon(obj, x, y, z) {
-		var geometry = new THREE.CubeGeometry(10, 10, 35);
-		var mesh = new THREE.Mesh(geometry, material1);
-		mesh.position.set(x, y, z);
+	addASCanon ( obj, x, y, z ) {
+		var geometry = new THREE.CubeGeometry( 10, 10, 35 );
+		var mesh = new THREE.Mesh( geometry, material1 );
+		mesh.position.set( x, y, z );
 
-		obj.add(mesh);
+		obj.add( mesh );
 	}
 
-	addASGun(obj, x, y, z) {
-		var geometry = new THREE.CubeGeometry(4, 10, 36);
-		var mesh = new THREE.Mesh(geometry, material1);
-		mesh.position.set(x, y, z);
+	addASGun ( obj, x, y, z ) {
+		var geometry = new THREE.CubeGeometry( 4, 10, 36 );
+		var mesh = new THREE.Mesh( geometry, material1 );
+		mesh.position.set( x, y, z );
 
-		obj.add(mesh);
+		obj.add( mesh );
 	}
 
-	addASGunCockpit1(obj, x, y, z) {
+	addASGunCockpit1 ( obj, x, y, z ) {
 		var triangleShape = new THREE.Shape();
-		triangleShape.moveTo(0, 0);
-		triangleShape.lineTo(0, 16);
-		triangleShape.lineTo(36, 0);
-		triangleShape.lineTo(0, 0);
+		triangleShape.moveTo( 0, 0 );
+		triangleShape.lineTo( 0, 16 );
+		triangleShape.lineTo( 36, 0 );
+		triangleShape.lineTo( 0, 0 );
 
-		var geometry = new THREE.ExtrudeGeometry(triangleShape, { amount: 10, bevelEnabled: false });
-		var mesh = new THREE.Mesh(geometry, material1);
+		var geometry = new THREE.ExtrudeGeometry( triangleShape, { amount: 10, bevelEnabled: false } );
+		var mesh = new THREE.Mesh( geometry, material1 );
 		mesh.rotation.x = -Math.PI/2;
 		mesh.rotation.z = Math.PI/2;
-		mesh.position.set(x, y, z);
+		mesh.position.set( x, y, z );
 
-		obj.add(mesh);
+		obj.add( mesh );
 	}
 
-	addASGunCockpit2(obj, x, y, z) {
+	addASGunCockpit2 ( obj, x, y, z ) {
 		var triangleShape = new THREE.Shape();
-		triangleShape.moveTo(0, 0);
-		triangleShape.lineTo(0, 16);
-		triangleShape.lineTo(36, 0);
-		triangleShape.lineTo(0, 0);
+		triangleShape.moveTo( 0, 0 );
+		triangleShape.lineTo( 0, 16 );
+		triangleShape.lineTo( 36, 0 );
+		triangleShape.lineTo( 0, 0 );
 
-		var geometry = new THREE.ExtrudeGeometry(triangleShape, { amount: -10, bevelEnabled: false });
-		var mesh = new THREE.Mesh(geometry, material1);
+		var geometry = new THREE.ExtrudeGeometry( triangleShape, { amount: -10, bevelEnabled: false } );
+		var mesh = new THREE.Mesh( geometry, material1 );
 		mesh.rotation.x = Math.PI/2;
 		mesh.rotation.z = -Math.PI/2;
-		mesh.position.set(x, y, z);
+		mesh.position.set( x, y, z );
 
-		obj.add(mesh);
+		obj.add( mesh );
 	}
 
-	addASLeg(obj, x, y, z) {
-		var geometry = new THREE.CubeGeometry(8, 2, 8);
-		var mesh = new THREE.Mesh(geometry, material1);
-		mesh.position.set(x, (y - 5), z);
+	addASLeg ( obj, x, y, z ) {
+		var geometry = new THREE.CubeGeometry( 8, 2, 8 );
+		var mesh = new THREE.Mesh( geometry, material1 );
+		mesh.position.set( x, (y - 5), z );
 
-		var geometry1 = new THREE.CylinderGeometry(4, 4, 8, 40, 8, false);
-		var mesh1 = new THREE.Mesh(geometry1, material1);
-		mesh1.position.set(x, y, z);
+		var geometry1 = new THREE.CylinderGeometry( 4, 4, 8, 40, 8, false );
+		var mesh1 = new THREE.Mesh( geometry1, material1 );
+		mesh1.position.set( x, y, z );
 
-		obj.add(mesh);
-		obj.add(mesh1);
+		obj.add( mesh );
+		obj.add( mesh1 );
 	}
 
-	move(interval) {
-		if(this.direction == "left") {
-			this.velocity -= (this.acceleration - this.friction) * interval;
+	move ( interval ) {
+		if ( this.direction == "left" ) {
+			this.velocity -= ( this.acceleration - this.friction ) * interval;
 		}
 
-		if(this.direction == "right") {
-			this.velocity += (this.acceleration - this.friction)  * interval;
+		if ( this.direction == "right" ) {
+			this.velocity += ( this.acceleration - this.friction )  * interval;
 		}
 
-		if(this.direction == "none") {
-			if(this.velocity < 0) {
-				this.velocity += (this.friction * 2) * interval;
-				if(this.velocity > 0) {
+		if ( this.direction == "none" ) {
+			if ( this.velocity < 0 ) {
+				this.velocity += ( this.friction * 2 ) * interval;
+				if ( this.velocity > 0 ) {
 					this.velocity = 0;
 				}
 			}
 
-			if(this.velocity > 0) {
-				this.velocity -= (this.friction * 2) * interval;
-				if(this.velocity < 0) {
+			if ( this.velocity > 0 ) {
+				this.velocity -= ( this.friction * 2 ) * interval;
+				if ( this.velocity < 0 ) {
 					this.velocity = 0;
 				}
 			}
@@ -143,14 +143,14 @@ class AlliedShip {
 		this.collision();
 	}
 
-	collision() {
-		if(this.AS.position.x > 910) {
+	collision () {
+		if ( this.AS.position.x > 910 ) {
 			this.AS.position.x = 910;
 			this.velocity = 0;
 			this.direction = "none";
 		}
 
-		if(this.AS.position.x < -910) {
+		if ( this.AS.position.x < -910 ) {
 			this.AS.position.x = -910;
 			this.velocity = 0;
 			this.direction = "none";
--- js/field.js
+++ js/field.js
@@ -2,49 +2,49 @@
  * CG Space Invaders
  * CG45179 16'17
  *
- * @author: Rui Ventura (ist181045)
- * @author: Diogo Freitas (ist181586)
- * @author: Sara Azinhal (ist181700)
+ * @author: Rui Ventura ( ist181045 )
+ * @author: Diogo Freitas ( ist181586 )
+ * @author: Sara Azinhal ( ist181700 )
  */
 
 class Field {
 
-	constructor(x, y, z) {
+	constructor( x, y, z ) {
 		var field = new THREE.Object3D();
 
-		this.addBottom(field, 0, -75, 0);
-		this.addVerticalBarriers(field, -975, 0, 0);
-		this.addVerticalBarriers(field, 975, 0, 0);
-		this.addHorizontalBarriers(field, 0, 0, -725);
-		this.addHorizontalBarriers(field, 0, 0, 725);
+		this.addBottom( field, 0, -75, 0 );
+		this.addVerticalBarriers( field, -975, 0, 0 );
+		this.addVerticalBarriers( field, 975, 0, 0 );
+		this.addHorizontalBarriers( field, 0, 0, -725 );
+		this.addHorizontalBarriers( field, 0, 0, 725 );
 
-		scene.add(field);
+		scene.add( field );
 		field.position.x = x;
 		field.position.y = y;
 		field.position.z = z;
 	}
 
-	addBottom(obj, x, y, z) {
-		var geometry = new THREE.CubeGeometry(2000, 1, 1500);
-		var mesh = new THREE.Mesh(geometry, material4);
-		mesh.position.set(x, y, z);
+	addBottom( obj, x, y, z ) {
+		var geometry = new THREE.CubeGeometry( 2000, 1, 1500 );
+		var mesh = new THREE.Mesh( geometry, material4 );
+		mesh.position.set( x, y, z );
 
-		obj.add(mesh);
+		obj.add( mesh );
 	}
 
-	addVerticalBarriers(obj, x, y, z) {
-		var geometry = new THREE.CubeGeometry(50, 150, 1500);
-		var mesh = new THREE.Mesh(geometry, material5);
-		mesh.position.set(x, y, z);
+	addVerticalBarriers( obj, x, y, z ) {
+		var geometry = new THREE.CubeGeometry( 50, 150, 1500 );
+		var mesh = new THREE.Mesh( geometry, material5 );
+		mesh.position.set( x, y, z );
 
-		obj.add(mesh);
+		obj.add( mesh );
 	}
 
-	addHorizontalBarriers(obj, x, y, z) {
-		var geometry = new THREE.CubeGeometry(2000, 150, 50);
-		var mesh = new THREE.Mesh(geometry, material5);
-		mesh.position.set(x, y, z);
+	addHorizontalBarriers( obj, x, y, z ) {
+		var geometry = new THREE.CubeGeometry( 2000, 150, 50 );
+		var mesh = new THREE.Mesh( geometry, material5 );
+		mesh.position.set( x, y, z );
 
-		obj.add(mesh);
+		obj.add( mesh );
 	}
 }
