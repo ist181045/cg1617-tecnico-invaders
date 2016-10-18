4a87b5fcf9feb4cce3694252bfb7017413633cf1
Rui Ventura <RGCV@users.noreply.github.com>
2016-10-18 16:54:03 +0100

    Redo Field class (resize inbound)

--- js/field.js
+++ js/field.js
@@ -1,50 +1,106 @@
+/*global THREE*/
+
 /**
  * CG Space Invaders
- * CG45179 16'17
+ * Game field - CG45179 16'17
  *
  * @author: Rui Ventura ( ist181045 )
  * @author: Diogo Freitas ( ist181586 )
  * @author: Sara Azinhal ( ist181700 )
  */
 
-class Field {
+class Field extends THREE.Object3D {
 
-	constructor( x, y, z ) {
-		var field = new THREE.Object3D();
+	constructor( x, y, z, w = 250, l = 250 ) {
 
-		this.addBottom( field, 0, -75, 0 );
-		this.addVerticalBarriers( field, -975, 0, 0 );
-		this.addVerticalBarriers( field, 975, 0, 0 );
-		this.addHorizontalBarriers( field, 0, 0, -725 );
-		this.addHorizontalBarriers( field, 0, 0, 725 );
+		super();
 
-		scene.add( field );
-		field.position.x = x;
-		field.position.y = y;
-		field.position.z = z;
+		this.type = 'Field';
+
+		this._width  = w;
+		this._length = l;
+
+		this._buildField( 0, 0, 0 );
+
+		this.position.set( x, y, z );
+
 	}
 
-	addBottom( obj, x, y, z ) {
-		var geometry = new THREE.CubeGeometry( 2000, 1, 1500 );
-		var mesh = new THREE.Mesh( geometry, material4 );
+	get width () {
+
+		return this._width;
+
+	}
+
+	get length () {
+
+		return this._length;
+
+	}
+
+	_buildField ( x, y, z ) {
+
+		let w = this._width;
+		let l = this._length;
+
+		console.log( `${w} : ${l}` );
+
+		let barriers = new THREE.Group();
+
+		let barrierMaterial = new THREE.MeshBasicMaterial( {
+			color: new THREE.Color( 0xFFFFFF )
+		} );
+
+		let floorMaterial = new THREE.MeshBasicMaterial( {
+			color: new THREE.Color( 0x1A1A1A ),
+			side: THREE.DoubleSide
+		} );
+
+		this.children.splice( 0, this.children.length );
+
+		this._addFloor( this, x, y, z, floorMaterial );
+		this._addVerticalBarrier( barriers, x + w / -2, y, z, barrierMaterial );
+		this._addVerticalBarrier( barriers, x + w /  2, y, z, barrierMaterial );
+		this._addHorizontalBarrier( barriers, x, y, z + l / -2, barrierMaterial );
+		this._addHorizontalBarrier( barriers, x, y, z + l /  2, barrierMaterial );
+
+		this.add( barriers );
+
+	}
+
+
+	_addFloor ( obj, x, y, z, material ) {
+
+		let geometry = new THREE.PlaneGeometry( this._width, this._length );
+		let mesh = new THREE.Mesh( geometry, material );
+
 		mesh.position.set( x, y, z );
+		mesh.rotateX( Math.PI / -2 );
+		
+		obj.add( mesh );
+
+	}
+
+	_addVerticalBarrier ( obj, x, y, z, material ) {
+
+		let geometry = new THREE.CubeGeometry( 4, 2, this._length );
+		let mesh = new THREE.Mesh( geometry, material );
+
+		mesh.position.set( x > 0 ? x - 2 : x + 2, y, z );
 
 		obj.add( mesh );
+
 	}
 
-	addVerticalBarriers( obj, x, y, z ) {
-		var geometry = new THREE.CubeGeometry( 50, 150, 1500 );
-		var mesh = new THREE.Mesh( geometry, material5 );
-		mesh.position.set( x, y, z );
+	_addHorizontalBarrier ( obj, x, y, z, material ) {
+
+		let geometry = new THREE.CubeGeometry( this._width, 2, 4 );
+		let mesh = new THREE.Mesh( geometry, material );
+
+		mesh.position.set( x, y, z > 0 ? z - 2 : z + 2 );
 
 		obj.add( mesh );
+
 	}
 
-	addHorizontalBarriers( obj, x, y, z ) {
-		var geometry = new THREE.CubeGeometry( 2000, 150, 50 );
-		var mesh = new THREE.Mesh( geometry, material5 );
-		mesh.position.set( x, y, z );
-
-		obj.add( mesh );
-	}
 }
