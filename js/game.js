c9ec7a6a57f242546a4388b55e3062ba37fb4223
Diogo Freitas <diogo.maria.freitas@gmail.com>
2016-10-18 16:12:14 +0100

    StaticBackCamera and DynamicBackCamera Implemented

--- js/game.js
+++ js/game.js
@@ -17,12 +17,13 @@ var material5 = new THREE.MeshBasicMaterial({ color: 0xffffff, wireframe: false 
 
 //GLOBAL VARIABLES
 
-var camera1, camera2;
+var camera1, camera2, camera3;
 var camera, scene, renderer;
 var clock = new THREE.Clock(true);
 var AShip;
 
 var X = 1920, Y = 1080;
+var CAMERA_MOVEMENT = false;
 
 //CAMERAS
 
@@ -35,20 +36,21 @@ function createOrtographicCamera() {
 }
 
 function createStaticBackCamera() {
-	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 2000);
 	camera.position.x = 0;
-	camera.position.y = 300;
+	camera.position.y = 700;
 	camera.position.z = 900;
 	camera.lookAt(scene.position);
 	camera.updateProjectionMatrix();
 }
 
 function createDynamicBackCamera() {
-	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 1000);
-	camera.position.x = 0;
-	camera.position.y = 0;
-	camera.position.z = 700;
-	camera.lookAt(scene.position);
+	camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 2000);
+	camera.position.x = AShip.AS.position.x;
+	camera.position.y = 50;
+	camera.position.z = 725;
+	camera.lookAt(AShip.AS.position);
+	camera.updateProjectionMatrix();
 }
 
 //SCENES
@@ -134,12 +136,15 @@ function onKeyDown(e) {
 	  		console.log("Arrow Right - onKeyDown");
 			break;
 		case 49:
+			CAMERA_MOVEMENT = false;
 			createOrtographicCamera();
 			break;
 		case 50:
+			CAMERA_MOVEMENT = false;
 			createStaticBackCamera();
 			break;
 		case 51:
+			CAMERA_MOVEMENT = true;
 			createDynamicBackCamera();
 			break;
 		case 77:
@@ -150,24 +155,6 @@ function onKeyDown(e) {
 			break;
  	}
 }
-
-/*function onResize(e) {
-	var screenratio = window.innerWidth / window.innerHeight;
-	var renderratio = renderer.getSize().width / renderer.getSize().height;
-	
-	if(screenratio / renderratio > 1) {
-		camera.left = -(Y/2) * screenratio;
-		camera.right = (Y/2) * screenratio;
-	}
-	if(screenratio / renderratio < 1) {
-		camera.top = (X/2) / screenratio;
-		camera.bottom = -(X/2) / screenratio;
-	}
-
-	camera.aspect = screenratio;
-	camera.updateProjectionMatrix();
-	renderer.setSize(window.innerWidth, window.innerHeight);
-}*/
 
 function animate() {
 	var interval = clock.getDelta();
