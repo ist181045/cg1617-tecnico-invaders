4282b93c078d10d68fab689470ccc1227ae2243c
Diogo Freitas <diogo.maria.freitas@gmail.com>
2016-10-28 08:15:49 +0100

    Minor updates to Game.js

--- js/bullet.js
+++ js/bullet.js
@@ -17,8 +17,14 @@ class Bullet extends THREE.Object3D {
 		this.direction = new THREE.Vector2(GameField.AShip.pointingDir[1].x, GameField.AShip.pointingDir[1].y);
 		this.direction.normalize();
 
+		var pts = [new THREE.Vector3(0, 2, 0), new THREE.Vector3(0.5, 1.5, 0.5), new THREE.Vector3(0.75, 1, 1), 
+				   new THREE.Vector3(0.90, 0.5, 1.5), new THREE.Vector3(1, 0, 2), new THREE.Vector3(1, -0.5, 2.5),
+				   new THREE.Vector3(1, -1, 3), new THREE.Vector3(1, -1.5, 3.5), new THREE.Vector3(1, -2, 4)];
+
 		var geometry = new THREE.CubeGeometry (1, 1, 4);
+		//var geometry = new THREE.LatheGeometry(pts, 50, 0, 2 * Math.PI);
 		var mesh = new THREE.Mesh( geometry , material1 );
+		//mesh.rotation.x = -Math.PI/2;
 
 		mesh.position.set( x, y, z );
 		mesh.rotation.y = GameField.AShip.rotation.y;
