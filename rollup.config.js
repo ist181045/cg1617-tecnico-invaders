8fd7eb2df9d16741964186f9c6829f5e17281d51
Rui Ventura <rventura.pt@outlook.com>
2016-11-03 19:45:49 +0000

    Remove es6 bundling, now bundles with separate three.js files

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,46 @@
 import babel from 'rollup-plugin-babel';
 
+/**
+ * From three.js rollup.config.js
+ *
+ * @see https://github.com/mrdoob/three.js/blob/r82/rollup.config.js
+ */
+
+function glsl () {
+	return {
+		transform ( code, id ) {
+			if ( !/\.glsl$/.test( id ) ) return;
+
+			var transformedCode = 'export default ' + JSON.stringify(
+				code
+					.replace( /[ \t]*\/\/.*\n/g, '' )
+					.replace( /[ \t]*\/\*[\s\S]*?\*\//g, '' )
+					.replace( /\n{2,}/g, '\n' )
+			) + ';';
+			return {
+				code: transformedCode,
+				map: { mappings: '' }
+			};
+		}
+	};
+}
+
 export default {
 	entry: 'src/Game.js',
 	plugins: [
+		glsl(),
 		babel({
-			exclude: 'node_modules/**'
+			exclude: [
+				'node_modules/**',
+				'src/lib/**'
+			]
 		})
 	],
 	targets: [
 		{
-			format: 'es',
-			dest: 'dist/invaders.es6.js'
-		},
-		{
 			format: 'iife',
-			dest: 'dist/invaders.js'
+			dest: 'dist/invaders.js',
+			moduleName: 'invaders'
 		}
 	]
 };
