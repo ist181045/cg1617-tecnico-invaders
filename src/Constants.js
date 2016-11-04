a5a57934eb6aa93ee7aa5d055f3c37a104f650a2
Rui Ventura <rventura.pt@outlook.com>
2016-11-04 02:47:12 +0000

    Add constant holding files

---
+++ src/Constants.js
@@ -0,0 +1,16 @@
+/**
+ * CG Space Invaders
+ * CG45179 16'17
+ *
+ * @author: Rui Ventura ( ist181045 )
+ * @author: Diogo Freitas ( ist181586 )
+ * @author: Sara Azinhal ( ist181700 )
+ */
+
+export const WIDTH  = 800;
+export const HEIGHT = 600;
+
+export const WINDOW_PIXEL_RATIO = window.devicePixelRatio;
+
+export const WINDOW_WIDTH  = () => window.innerWidth;
+export const WINDOW_HEIGHT = () => window.innerHeight;
